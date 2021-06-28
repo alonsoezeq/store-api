bf52935acaec4d4c708a1e859696b1c3de8e3ec6
Braian Cardozo <bcardozo@quadiontech.com>
2021-06-28 00:04:13 -0300

    Agrego para consumir con postgre

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   //connectionUri: 'postgres://user:pass@example.com:5432/dbname',
-  connectionUri: 'sqlite::memory:',
+  connectionUri: 'postgres://dkuzuxidecsfzo:9ff9f6b7fd3552d676358f39a1dc0585eacfb5bc42b3811de5036273d43c233f@ec2-35-171-250-21.compute-1.amazonaws.com:5432/d2rj75f76vmt5p',
   //connectionUri: 'mysql://user:pass@example.com:5432/dbname',
   basePath: '/api/v1',
   defaultPageItems: 15,
